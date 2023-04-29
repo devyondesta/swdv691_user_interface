d39e7a53e201be312cd610975ebf7796d5847a22
devyondesta <yonasdesta@ymail.com>
2023-04-29 13:14:02 -0400

    Milestone 6 Sprint 3 Login SignUp Database

--- project/assets/js/script.js
+++ project/assets/js/script.js
@@ -1,23 +1,28 @@
-$(document).ready(function() {
-    $('.logo-carousel').slick({
-      slidesToShow: 8,
-      slidesToScroll: 1,
-      autoplay: true,
-      autoplaySpeed: 1000,
-      arrows: true,
-      dots: false,
-      pauseOnHover: false,
-      responsive: [{
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 4
-        }
-      }, {
-        breakpoint: 520,
-        settings: {
-          slidesToShow: 2
-        }
-      }]
-    });
+$(document).ready(function () {
+  $('.logo-carousel').slick({
+    slidesToShow: 8,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    arrows: true,
+    dots: false,
+    pauseOnHover: false,
+    responsive: [{
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 4
+      }
+    }, {
+      breakpoint: 520,
+      settings: {
+        slidesToShow: 2
+      }
+    }]
   });
-  
+});
+
+$("#search_form").submit(function (event) {
+  event.preventDefault();
+  var search_query = $("#search").val();
+  window.location.href = "search.html?q=" + search_query;
+});
